055e528f9e87456872a57656c453dd8badb86dc7
Krishnesh <krishneshks97@gmail.com>
2025-02-18 23:28:11 +0530

    Live

---
+++ docs/sidebar/sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../app/shared.service';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent implements OnInit {
+  data: string = '';
+  constructor(private dataService: SharedService) {}
+  ngOnInit() {
+    this.dataService.data$.subscribe(data => {
+      this.data = data; 
+    });
+    this.dataService.data3$.subscribe(data => {
+      this.data = data; 
+    }); 
+  }
+}
